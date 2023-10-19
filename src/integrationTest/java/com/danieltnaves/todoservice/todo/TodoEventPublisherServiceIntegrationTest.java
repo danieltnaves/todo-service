9a10fa0a6f129cd9570422b6f5d64035b8dc1c17
Daniel Naves <daniel.naves@outlook.com>
2023-10-19 21:21:38 +0200

    Add integration test case to check the event publisher component and the past due event publication

---
+++ src/integrationTest/java/com/danieltnaves/todoservice/todo/TodoEventPublisherServiceIntegrationTest.java
@@ -0,0 +1,37 @@
+package com.danieltnaves.todoservice.todo;
+
+import static com.danieltnaves.todoservice.todo.domain.Todo.*;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.awaitility.Awaitility.await;
+
+import com.danieltnaves.todoservice.todo.domain.Todo;
+import com.danieltnaves.todoservice.todo.events.TodoEventPublisherService;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TodoEventPublisherServiceIntegrationTest {
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+    @Autowired
+    private TodoEventPublisherService todoEventPublisherService;
+
+    @Test
+    void testPublishPastDueEvent() {
+        Todo todo = builder()
+                .description("This a past due item with the status not yet updated to past due")
+                .status(Status.NOT_DONE)
+                .createdAt(LocalDateTime.now())
+                .dueAt(LocalDateTime.now().minusDays(1))
+                .build();
+        todoRepository.save(todo);
+        todoEventPublisherService.publishUpdatePastDueEvent(todo);
+        Status updatedStatus = todoRepository.findById(todo.getId()).orElseThrow(RuntimeException::new).getStatus();
+        await().atMost(5, SECONDS).until(() -> Status.PAST_DUE.equals(updatedStatus));
+    }
+
+}
